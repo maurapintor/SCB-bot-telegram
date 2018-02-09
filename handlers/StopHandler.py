99e7b53e2a0192373b65d586704e388894747ba6
Marco Uras <marco.uras2@gmail.com>
2018-02-09 16:36:57 +0100

    Stop Handler

--- handlers/StopHandler.py
+++ handlers/StopHandler.py
@@ -9,6 +9,7 @@ from utils.telegramMsg import  sendToAll
 class StopHandler(webapp2.RequestHandler):
     def post(self):
         api_key = self.request.get('apiKey')
+
         logging.warning("PositionRequestHandler, key: {}".format(api_key))
 
         api_key = 'prova'
