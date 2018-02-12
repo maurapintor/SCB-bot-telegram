db66b5493d3eac4a152baa69ab7d856618623959
Marco Uras <marco.uras2@gmail.com>
2018-02-12 13:29:44 +0100

    Stop Handler

--- handlers/StopHandler.py
+++ handlers/StopHandler.py
@@ -2,19 +2,22 @@ import logging
 
 import webapp2
 
-from utils.telegramMsg import  sendToAll
-
+from utils.telegramMsg import sendToAll, sendLocationToAll
 
 
 class StopHandler(webapp2.RequestHandler):
     def post(self):
         api_key = self.request.get('apiKey')
+        latitude = self.request.get('latitude')
+        longitude = self.request.get('longitude')
 
-        logging.warning("PositionRequestHandler, key: {}".format(api_key))
+        logging.warning("StopHandler, key: {}".format(api_key))
+        logging.warning("StopHandler, lat {} | lon {}".format(latitude,longitude))
 
         api_key = 'prova'
         if api_key == 'prova':
             sendToAll( "Pare che il veicolo si sia fermato.")
+            sendLocationToAll(latitude,longitude)
 
         else:
             logging.warning(
