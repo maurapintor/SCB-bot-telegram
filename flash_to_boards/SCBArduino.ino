8e16f6dc1501d001df28841ef2f00abc78ca1877
maurapintor <maura.pintor@gmail.com>
2018-02-28 18:54:04 +0100

    Corrected Arduino file

--- flash_to_boards/SCBArduino.ino
+++ flash_to_boards/SCBArduino.ino
@@ -1,322 +1,226 @@
-
-
-#include <SoftwareSerial.h>
-#include <TinyGPS.h>
-#include <ESP8266WiFi.h>
-#include <WiFiClientSecure.h>
-#include <PubSubClient.h>
-
 /*
-                     ----Made by Pizza-Team----
-       This sketch detects movement through an accelerometer connected to Arduino
-       and sends to serial monitor of NodeMCU the GPS coordinates when triggered
-       (communication through alarmPin). Then it sends the values to the google datastore.
-       IMPORTANT: set the connection variables before upload!
+MMA7361 ACCELEROMETER:
+  X --> A5
+  Y --> A4
+  Z --> A3
+  3V --> 3.3V
+  GND --> GND
+  G-SEL --> GND
+  SLEEP --> 3.3
+  AREF ARDUINO --> 3.3V ARDUINO
 */
 
-//---------------------------------------------------------------------------------//
 
-/*
-   Connection settings
-*/
+//Global Variables and constants
+int buttonPin = HIGH; // button Pin connected to Analog 0
+int alarmPin = 4;
 
-char ssid[]     = "";    // your network SSID (name)
-char password[] = "";    // network key
-String yourProjectName = "";
-const char* mqtt_server = "tools.lysis-iot.com"; // you can use this one if you want, it should work
-const char* topic = "";
-
-//---------------------------------------------------------------------------------//
+//Accelerometer Pins
+const int x = A3; // X pin connected to Analog 3
+const int y = A4; // Y pin connected to Analog 4
+const int z = A5; // Z pin connected to Analog 5
 
 
-/*
-       GPS settings
-*/
-TinyGPS gps;
-SoftwareSerial ss(4, 2);
+//Alarm LED
+const int ledPin = 2; // LED connected to Digital 2
 
-static void smartdelay(unsigned long ms);
-static String print_date(TinyGPS &gps);
-bool flag = 0;
-
-float flat, flon;
-unsigned long age, date, tempo, chars = 0;
-String data;
-
-//---------------------------------------------------------------------------------//
-
-/*
-   mqtt settings
-*/
-WiFiClient espClient;
-PubSubClient mqttclient(espClient);
-long lastMsg = 0;
-char msg[50];
-int value = 0;
-
-
-/*
-   Pin settings
-*/
-int alarmPin = 13;
-int active = LOW;
-int ledPin = LED_BUILTIN;
-//---------------------------------------------------------------------------------//
-
-
-String apiKey = "prova";
-const char * host = ".appspot.com";
-String server_name = yourProjectName + host;
-const char *server = server_name.c_str();
+int tolerance= 70; // Sensitivity of the Alarm
+boolean calibrated=false; // When accelerometer is calibrated - changes to true 
+boolean moveDetected=false; // When motion is detected - changes to true
 
 
 
-int sent = 0;
-unsigned long last_req = 0;
-int sent_stop = 1;
+//Accelerometer limits
+int xMin; //Minimum x Value
+int xMax; //Maximum x Value
+int xVal; //Current x Value
+
+int yMin; //Minimum y Value
+int yMax; //Maximum y Value
+int yVal; //Current y Value
+
+int zMin; //Minimum z Value
+int zMax; //Maximum z Value
+int zVal; //Current z Value
 
 
-void setup() {
-  //Monitor output
-  Serial.begin(38400);
-  pinMode(alarmPin, INPUT);
 
-  //GPS output
-  ss.begin(9600);
-
-  //Wi-fi output
-  Serial.print("Connecting Wifi: ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
-  delay(500);
-  pinMode(ledPin, OUTPUT);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    digitalWrite(ledPin, !digitalRead(ledPin));     // set pin to the opposite state
-    delay(500);
-  }
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  IPAddress ip = WiFi.localIP();
-  Serial.println(ip);
-  digitalWrite(ledPin, HIGH);
-
-  mqttclient.setServer(mqtt_server, 1883);
-  mqttclient.setCallback(callback);
-}
-
-void loop() {
-
-  active = digitalRead(alarmPin);
-  if (active == HIGH) {
-    printPosition(false);
-    last_req = millis();
-    sent_stop = 0;
-  }
-
-  else{
-  if (millis()-last_req > 180000 && !sent_stop){
-    sendStop();
-    sent_stop = 1;
-    last_req = millis();
-  }
-  }
-  Serial.println("last_req = " + String(last_req) + "\t millis = " + String(millis()));
-  delay(10);
-  if (!mqttclient.connected()) {
-    reconnect();
-  }
-  mqttclient.loop();
-  delay(10);
+void setup(){
+ //Begin Serial communication
+ Serial.begin(38400);
+ 
+ //Initilise LED Pin
+ pinMode(ledPin, OUTPUT);
+ pinMode(alarmPin, OUTPUT);
+ analogWrite(alarmPin, LOW);    // sets the LED to the button's value
 
 }
 
 
-//---------------------------------------------------------------------------------//
 
-
-/*
-   GPS Functions
-*/
-void printPosition(bool is_requested) {
-  if (flag) {
-    gps.f_get_position(&flat, &flon, &age);
-    float fspeed = gps.f_speed_kmph();
-
-    Serial.println(flat, 6);
-    Serial.println(flon, 6);
-    Serial.println(fspeed, 6);
-    data = print_date(gps);
-    Serial.println();
-    sendPosition(flat, flon, fspeed, data, is_requested);
-    smartdelay(1000);
-  }
-  else {
-    gps.f_get_position(&flat, &flon, &age);
-    smartdelay(1000);
-    flag = 1;
-  }
+void loop(){
+ // If the button is pressed, initialise and recalibrate the Accelerometer limits.
+ if(buttonPin == HIGH){
+ calibrateAccel();
+ buttonPin = LOW;
+ }
+ 
+ // Once the accelerometer is calibrated - check for movement 
+ 
+ if(checkMotion()){
+ moveDetected=true;
+ }
+ 
+ // If motion is detected - sound the alarm !
+ if(moveDetected){
+ Serial.println("ALARM");
+ ALARM();
+ delay(100);
+ moveDetected = false;
+ }
+ 
 }
 
 
-void sendPosition(float lat, float lon, float fspeed, String(data), bool is_requested)
-{
-  WiFiClient wificlient;
-
-  if (wificlient.connect(server, 80)) {
-    Serial.println("WiFi Client connected, sending data ");
-
-    String postStr = "latitude=";
-    postStr += String(lat, 6);
-    postStr += "&longitude=";
-    postStr += String(lon, 6);
-    postStr += "&speed=";
-    postStr += String(fspeed, 6);
-    postStr += "&data=";
-    postStr += data;
-    postStr += "&apiKey=";
-    postStr += apiKey;
-    postStr += "&requested=";
-    postStr += String(is_requested);
-    Serial.println(is_requested);
-    Serial.println(String(is_requested));
-
-    postStr += "\r\n\r\n";
-    String coords = String(lat, 6) + "," + String(lon, 6);
-
-    wificlient.print("POST /position/put HTTP/1.1\n");
-    wificlient.print("Host: "+yourProjectName+".appspot.com\n");
-    wificlient.print("Connection: close\n");
-    wificlient.print("Content-Type: application/x-www-form-urlencoded\n");
-    wificlient.print("Content-Length: ");
-    //   wificlient.print("apiKey: " + apiKey + "\n");
-
-    wificlient.print(postStr.length());
-    wificlient.print("\n\n");
-    wificlient.print(postStr);
-    delay(1000);
+//This is the function used to sound the buzzer
+void buzz(int reps, int rate){
+ for(int i=0; i<reps; i++){
+ delay(100);
+ delay(rate);
+ }
+} 
 
 
-    while (wificlient.available()) {
-      String line = wificlient.readStringUntil('\r');
-      Serial.print(line);
-    }
-    Serial.println();
-    Serial.println("closing connection");
-
-  }//end if
 
 
-  sent++;
-  wificlient.stop();
-  delay(5000);
+// Function used to calibrate the Accelerometer
+void calibrateAccel(){
+ // reset alarm
+ moveDetected=false;
+ 
+ //initialise x,y,z variables
+ xVal = analogRead(x);
+ xMin = xVal;
+ xMax = xVal;
+ 
+ yVal = analogRead(y);
+ yMin = yVal;
+ yMax = yVal;
+ 
+ zVal = analogRead(z);
+ zMin = zVal;
+ zMax = zVal;
+ 
+ // Calibration sequence initialisation sound - 3 seconds before calibration begins
+ buzz(3,1000);
+ 
+ //calibrate the Accelerometer (should take about 0.5 seconds)
+ for (int i=0; i<50; i++){
+ // Calibrate X Values
+ xVal = analogRead(x);
+ if(xVal>xMax){
+ xMax=xVal;
+ }else if (xVal < xMin){
+ xMin=xVal;
+ }
 
-}
+ // Calibrate Y Values
+ yVal = analogRead(y);
+ if(yVal>yMax){
+ yMax=yVal;
+ }else if (yVal < yMin){
+ yMin=yVal;
+ }
 
-static void smartdelay(unsigned long ms)
-{
-  unsigned long start = millis();
-  do
-  {
-    while (ss.available())
-      gps.encode(ss.read());
-  } while (millis() - start < ms);
-}
+ // Calibrate Z Values
+ zVal = analogRead(z);
+ if(zVal>zMax){
+ zMax=zVal;
+ }else if (zVal < zMin){
+ zMin=zVal;
+ }
 
-static String print_date(TinyGPS &gps)
-{
-  int year;
-  byte month, day, hour, minute, second, hundredths;
-  unsigned long age;
-  String data;
-  char sz[32];
-  gps.crack_datetime(&year, &month, &day, &hour, &minute, &second, &hundredths, &age);
-  if (age == TinyGPS::GPS_INVALID_AGE)
-    Serial.print("********** ******** ");
-  else
-  {
-
-    //sprintf(sz, "%02d/%02d/%02d %02d:%02d:%02d ",
-    //    month, day, year, hour, minute, second);
-    sprintf(sz, "%02d-%02d-%02d_%02d:%02d:%02d", month, day, year, hour, minute, second);
-    Serial.print(sz);
-  }
-  smartdelay(0);
-  data = String(sz);
-  return data;
-}
-
-/*
-   mqtt functions
-*/
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-
-  printPosition(true);
-
-}
-
-
-void reconnect() {
-  // Loop until we're reconnected
-  while (!mqttclient.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Create a random client ID
-    String clientId = "ESP8266Client-";
-    clientId += String(random(0xffff), HEX);
-    // Attempt to connect
-    if (mqttclient.connect(clientId.c_str())) {
-      Serial.println("connected");
-      mqttclient.subscribe(topic);
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(mqttclient.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
+ //Delay 10msec between readings
+ delay(10);
+ }
+ 
+ //End of calibration sequence sound. ARMED.
+ buzz(3,40);
+ printValues(); //Only useful when connected to computer- using serial monitor.
+ calibrated=true;
+ 
 }
 
 
 
-void sendStop()
-{   
-  WiFiClient wificlient;
+//Function used to detect motion. Tolerance variable adjusts the sensitivity of movement detected.
+boolean checkMotion(){
+ boolean tempB=false;
+ xVal = analogRead(x);
+ yVal = analogRead(y);
+ zVal = analogRead(z);
+ 
+ if(xVal >(xMax+tolerance)||xVal < (xMin-tolerance)){
+ tempB=true;
+ Serial.print("X Failed = ");
+ Serial.println(xVal);
+ }
+ 
+ if(yVal >(yMax+tolerance)||yVal < (yMin-tolerance)){
+ tempB=true;
+ Serial.print("Y Failed = ");
+ Serial.println(yVal);
+ }
+ 
+ if(zVal >(zMax+tolerance)||zVal < (zMin-tolerance)){
+ tempB=true;
+ Serial.print("Z Failed = ");
+ Serial.println(zVal);
+ }
+ 
+ return tempB;
+}
+ 
 
-  if (wificlient.connect(server, 80)) {
-    Serial.println("WiFi Client connected, sending data ");
 
-    String postStr = "apiKey=";
-    postStr += apiKey;
-    postStr += "&latitude=";
-    postStr += flat;
-    postStr += "&longitude=";
-    postStr += flon;
 
-    postStr += "\r\n\r\n";
 
-    wificlient.print("POST /position/stop HTTP/1.1\n");
-    wificlient.print("Host: "+yourProjectName+".appspot.com\n");
-    wificlient.print("Connection: close\n");
-    wificlient.print("Content-Type: application/x-www-form-urlencoded\n");
-    wificlient.print("Content-Length: ");
+// Prints the Sensor limits identified during Accelerometer calibration.
+// Prints to the Serial monitor.
+void printValues(){
+ Serial.print("xMin=");
+ Serial.print(xMin);
+ Serial.print(", xMax=");
+ Serial.print(xMax);
+ Serial.println();
+ 
+ Serial.print("yMin=");
+ Serial.print(yMin);
+ Serial.print(", yMax=");
+ Serial.print(yMax);
+ Serial.println();
+ 
+ Serial.print("zMin=");
+ Serial.print(zMin);
+ Serial.print(", zMax=");
+ Serial.print(zMax);
+ Serial.println();
+ 
+ Serial.println("------------------------");
+}
 
-    wificlient.print(postStr.length());
-    wificlient.print("\n\n");
-    wificlient.print(postStr);
-    delay(1000);
-  }
 
-  }
 
+
+//Function used to make the alarm sound, and blink the LED.
+void ALARM(){
+ //don't check for movement until recalibrated again
+ calibrated=false;
+ // sound the alarm and blink LED
+ digitalWrite(ledPin, HIGH);
+ buzz(4,20);
+ digitalWrite(ledPin, LOW);
+ digitalWrite(alarmPin, HIGH);    // sets the LED to the button's value
+ delay(100);
+ digitalWrite(alarmPin, LOW);    // sets the LED to the button's value
+}
